fdf4d35ddf274f0b49742263fb425d7a258897a8
Stuart Carter <Stuart.Carter@RKVTechnologies.com>
2022-02-17 11:28:04 -0600

    String interpolation in email service

--- AcmeApp/Acme.Common/EmailService.cs
+++ AcmeApp/Acme.Common/EmailService.cs
@@ -23,7 +23,7 @@ namespace Acme.Common
         {
             // Code to send an email
 
-            var confirmation = "Message sent: " + subject;
+            var confirmation = $"Message sent: {subject}";
             var loggingService = new LoggingService();
             loggingService.LogAction(confirmation);
             return confirmation;
